b927f975508cd47dfaf251bfe80c360db6825b63
Typhe <spayton681@gmail.com>
2026-03-17 23:04:40 +0200

    Update WorldGuardPermissionIntegration.kt

--- src/main/java/me/spartacus04/jext/integrations/WorldGuardPermissionIntegration.kt
+++ src/main/java/me/spartacus04/jext/integrations/WorldGuardPermissionIntegration.kt
@@ -7,7 +7,6 @@ import com.sk89q.worldguard.protection.flags.Flags
 import com.sk89q.worldguard.protection.flags.StateFlag
 import org.bukkit.block.Block
 import org.bukkit.entity.Player
-import org.bukkit.Bukkit
 
 
 internal class WorldGuardPermissionIntegration : PermissionIntegration {
@@ -21,16 +20,15 @@ internal class WorldGuardPermissionIntegration : PermissionIntegration {
 
     override fun hasJukeboxGuiAccess(player: Player, block: Block): Boolean = canInteract(player, block, Flags.CHEST_ACCESS, Flags.USE)
 
-    private fun canInteract(player: Player, block: Block, vararg flags: StateFlag): Boolean {
-		val wgPlayer = WorldGuardPlugin.inst().wrapPlayer(player)
-		val wgLocation = BukkitAdapter.adapt(block.location)
-		if(WorldGuard.getInstance().platform.sessionManager.hasBypass(wgPlayer, wgPlayer.world)) return true
-		val regionContainer = WorldGuard.getInstance().platform.regionContainer
-		val regions = regionContainer.createQuery().getApplicableRegions(wgLocation)
-		if(regions.size() == 0) return true  // no regions, allow it
-		val containerQuery = regionContainer.createQuery()
-		return flags.all {
-			containerQuery.testState(wgLocation, wgPlayer, it) != false
-		}
-	}
+    private fun canInteract(player: Player, block: Block, vararg flags: StateFlag) : Boolean {
+        val wgPlayer = WorldGuardPlugin.inst().wrapPlayer(player)
+        val wgLocation = BukkitAdapter.adapt(block.location)
+
+        if(WorldGuard.getInstance().platform.sessionManager.hasBypass(wgPlayer, wgPlayer.world)) return true
+
+        val containerQuery = WorldGuard.getInstance().platform.regionContainer.createQuery()
+        return flags.all {
+            containerQuery.testState(wgLocation, wgPlayer, it)
+        }
+    }
 }
